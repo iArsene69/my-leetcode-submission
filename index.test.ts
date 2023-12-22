bce1a719f95f4d7500653e79f539cb0f9a35035e
iArsene69 <mirzaprati@gmail.com>
2023-12-22 23:14:03 +0700

    263 264

--- index.test.ts
+++ index.test.ts
@@ -1,28 +1,58 @@
 import { describe, expect, test } from "bun:test";
-import { isHappy, maxScore } from ".";
+import { isHappy, isUgly, maxScore, nthUglyNumber } from ".";
 
 //202. Happy Number - Test
 describe("Happy Number", () => {
-    test("Happy number true", () => {
-        expect(isHappy(19)).toBe(true)
-    })
-    
-    test("Happy number false", () => {
-        expect(isHappy(2)).toBe(false)
-    })
-})
+  test("Happy number true", () => {
+    expect(isHappy(19)).toBe(true);
+  });
+
+  test("Happy number false", () => {
+    expect(isHappy(2)).toBe(false);
+  });
+});
 
 //1422. Maximum Score After Splitting a String - Test
 describe("Maximum Score", () => {
-    test("Ex 1", () => {
-        expect(maxScore("011101")).toBe(5)
-    })
+  test("Ex 1", () => {
+    expect(maxScore("011101")).toBe(5);
+  });
 
-    test("Ex 2", () => {
-        expect(maxScore("00111")).toBe(5)
-    })
+  test("Ex 2", () => {
+    expect(maxScore("00111")).toBe(5);
+  });
 
-    test("Ex 2", () => {
-        expect(maxScore("1111")).toBe(3)
-    })
-})
+  test("Ex 2", () => {
+    expect(maxScore("1111")).toBe(3);
+  });
+});
+
+//263. Ugly Number
+describe("Ugly number", () => {
+  test("Ex 1", () => {
+    expect(isUgly(6)).toBe(true);
+  });
+
+  test("Ex 2", () => {
+    expect(isUgly(1)).toBe(true);
+  });
+
+  test("Ex 3", () => {
+    expect(isUgly(14)).toBe(false);
+  });
+});
+
+//264. Ugly Number II
+describe("Ugly number the 2nd", () => {
+  test("Ex 1", () => {
+    expect(nthUglyNumber(10)).toBe(12);
+  });
+
+  test("Ex 2", () => {
+    expect(nthUglyNumber(1)).toBe(1);
+  });
+
+  test("Ex 3", () => {
+    expect(nthUglyNumber(1352)).toBe(402653184);
+  });
+});
--- index.ts
+++ index.ts
@@ -42,3 +42,35 @@ export function maxScore(s: string): number {
 
   return ones + score;
 }
+
+//263. Ugly Number
+export function isUgly(n: number): boolean {
+  const maxDivisions = (x: number, y: number) => {
+    while (x % y === 0) {
+      x /= y;
+    }
+    return x;
+  };
+  if (n == 0) return false;
+  n = maxDivisions(n, 2);
+  n = maxDivisions(n, 3);
+  n = maxDivisions(n, 5);
+
+  return n === 1;
+}
+
+//264. Ugly Number II
+export function nthUglyNumber(n: number): number {
+  const dp: number[] = new Array(n).fill(0);
+  dp[0] = 1;
+  let x = 0,
+    y = 0,
+    z = 0;
+  for (let i = 1; i < n; i++) {
+    dp[i] = Math.min(dp[x] * 2, Math.min(dp[y] * 3, dp[z] * 5));
+    if (dp[i] === 2 * dp[x]) x++;
+    if (dp[i] === 3 * dp[y]) y++;
+    if (dp[i] === 5 * dp[z]) z++;
+  }
+  return dp[n - 1];
+}
